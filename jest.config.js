f69ac3b425f23c519ecf7f3abdea22f31c1a8d91
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-27 14:17:35 -0300

    raw: jest config created.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bail: true,
+  testTimeout: 10000,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+}
