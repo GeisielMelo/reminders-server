2d87533eaac756536a57994db50cb97f52759e19
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-28 20:02:36 -0300

    raw: removed redis from the config file.

--- src/config.ts
+++ src/config.ts
@@ -8,12 +8,6 @@ export default {
   database: {
     url: process.env.DATABASE_URL,
   },
-  redis: {
-    localhost: process.env.REDIS_LOCALHOST_URL,
-    cloudHost: process.env.REDIS_CLOUD_HOST_URL,
-    cloudPassword: process.env.REDIS_CLOUD_HOST_PASSWORD,
-    cloudPort: Number(process.env.REDIS_CLOUD_HOST_PORT),
-  },
   mongo: {
     uri: process.env.MONGODB_URI,
   },
