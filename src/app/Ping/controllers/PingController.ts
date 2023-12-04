c89812327f1d2f45a24612b7a65b6e1b71e16f28
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-04 00:21:23 -0300

    fix: Fixed model to auto delete a document after 12 hours; also fixed index method on controllers to create a ping if there is no ping in the database.

--- src/app/Ping/controllers/PingController.ts
+++ src/app/Ping/controllers/PingController.ts
@@ -8,7 +8,19 @@ class PingController {
     try {
       await database.connect()
       const pings = await Ping.find()
-      if (!pings) throw new PingError('Error on list the pings.')
+
+      // if pings is a empty arr
+      if (pings.length === 0) {
+        try {
+          const ping = await Ping.create({
+            pingAt: Math.floor(Date.now() / 1000),
+          })
+          return res.status(200).json(ping)
+        } catch (error) {
+          throw new PingError('Error on ping creation.')
+        }
+      }
+
       return res.status(200).json(pings)
     } catch (error) {
       if (error instanceof PingError) return res.status(404).send()
--- src/database/models/Ping.ts
+++ src/database/models/Ping.ts
@@ -2,6 +2,7 @@ import { Schema, Document, model } from 'mongoose'
 
 interface IPing {
   pingAt: string
+  expireAt: Date
 }
 
 interface IPingDocument extends IPing, Document {}
@@ -9,9 +10,13 @@ interface IPingDocument extends IPing, Document {}
 const PingSchema = new Schema<IPingDocument>(
   {
     pingAt: { type: String, required: true },
+    expireAt: { type: Date, default: () => new Date(Date.now() + 43200 * 1000) },
   },
-  { timestamps: true, expires: 6 * 60 * 60 },
+  { timestamps: true },
 )
 
+PingSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 })
+
 const Ping = model<IPingDocument>('Ping', PingSchema)
+
 export default Ping
